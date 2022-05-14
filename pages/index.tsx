7f234d3e57fa2c2bfed4fd75a57ce4e8981cb43d
wakki0202 <ytk0202@outlook.jp>
2022-05-14 19:17:00 +0900

    first-commit

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { NextPage } from "next"
+import React from "react";
+import Header from "../components/Header";
+
+
+const Home: React.FC = () => {
+  return (
+    <div className=''>
+      <Header />
+
+    </div>
+  );
+}
+
+export default Home
---
+++ pages/service.tsx
@@ -0,0 +1,35 @@
+/* This example requires Tailwind CSS v2.0+ */
+import type { NextPage } from 'next'
+import Link from 'next/link';
+import Head from 'next/head';
+import { Fragment } from 'react'
+import Header from "../components/Header";
+
+
+// function classNames(...classes) {
+//   return classes.filter(Boolean).join(' ')
+// }
+
+
+const Home: NextPage = () => {
+  return (
+    <div className=''>
+      <Head>
+        <title>Create Next App</title>
+        <meta name='hogehogenyannyan' content='created by かめちゃん' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Header />
+
+      <main>
+        <div className='w-10/12 m-auto'>
+          <h1 className=' text-6xl  mt-12 font-bold'>Service.</h1>
+          <span className='text-1xl font-bold'>サービス</span>
+        </div>
+
+      </main>
+    </div>
+  );
+};
+
+export default Home
